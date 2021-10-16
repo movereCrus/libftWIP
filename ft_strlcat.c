71dfd0385ff192602bb05a2a5ddef146599317fa
movereCrus <vance200045@gmail.com>
2021-10-17 00:15:47 +0300

    Add strlcat strnstr

---
+++ ft_strlcat.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kirus <kirus@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/16 23:11:16 by kirus             #+#    #+#             */
+/*   Updated: 2021/10/16 23:11:35 by kirus            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+static size_t	st_strlen(const char *arr)
+{
+	size_t	c;
+
+	c = 0;
+	while (arr[c] != '\0')
+		c++;
+	return (c);
+}
+
+size_t	ft_strlcat(char *dest, const char *src, size_t size)
+{
+	size_t	eos;
+	size_t	len;
+	size_t	ctr;
+
+	eos = st_strlen(dest);
+	len = eos + st_strlen(src);
+	ctr = 0;
+	while (ctr < size - 1 && eos < size - 1 && src[ctr] != '\0')
+	{
+		dest[eos] = src[ctr];
+		ctr++;
+		eos++;
+	}
+	return (len);
+}
---
+++ ft_strnstr.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kirus <kirus@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/17 00:14:25 by kirus             #+#    #+#             */
+/*   Updated: 2021/10/17 00:14:40 by kirus            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+char	*ft_strnstr(const char *str, const char *to_find, size_t len)
+{
+	int	ptr1;
+	int	ptr2;
+
+	ptr1 = 0;
+	ptr2 = 0;
+	if (*to_find == 0)
+		return ((char *)str);
+	while (str[ptr1] != '\0' && len > 0)
+	{
+		while (to_find[ptr2] != '\0' && (to_find[ptr2] == str[ptr1]) && len > 0)
+		{
+			ptr2++;
+			ptr1++;
+			printf("%d %d\n", ptr1, ptr2);
+			len--;
+			if (to_find[ptr2] == '\0' && len >= 0)
+				return ((char *)&str[ptr1 - ptr2]);
+		}
+		ptr2 = 0;
+		ptr1++;
+		len--;
+	}
+	return (NULL);
+}
